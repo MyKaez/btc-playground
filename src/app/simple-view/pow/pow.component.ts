2b8b5be1789a53902611741a5038413cac297320
Kenneth Sean Newall <KennethSean.Newall@arvato.com>
2022-07-24 15:22:03 +0200

    added logic to simple pow

--- src/app/simple-view/pow/pow.component.ts
+++ src/app/simple-view/pow/pow.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { delay } from 'src/app/shared/delay';
-import { createBlockId } from 'src/app/simulators/helpers/block';
+import { PowService } from 'src/app/simulators/pow/pow.service';
 import { SimpleViewComponent } from '../simple-view.component';
 
 @Component({
@@ -9,9 +9,43 @@ import { SimpleViewComponent } from '../simple-view.component';
   styleUrls: ['../simple-view.component.scss', '../../materials.scss', '../../app.component.scss']
 })
 export class PowComponent {
+  private powService: PowService;
+  private isRunning: boolean;
+  public hashes: string[];
 
-  public simText: string = '';
-  public hashes: string[] = []
+  constructor() {
+    this.hashes = [];
+    this.isRunning = false;
+    this.powService = new PowService();
+  }
+
+  get hashRate(): number {
+    return this.powService.hashRate;
+  }
+
+  set hashRate(value: number) {
+    this.powService.hashRate = value;
+  }
+
+  get blockTime(): number {
+    return this.powService.blockTime;
+  }
+
+  set blockTime(value: number) {
+    this.powService.blockTime = value;
+  }
+
+  get expectedPrefixes(): string {
+    return this.powService.expectedPrefixes;
+  }
+
+  get expectedAmountOfBlocks(): number {
+    return this.powService.expectedAmountOfBlocks;
+  }
+
+  get validationInput(): [leadingZeros: number, probability: number] {
+    return this.powService.validationInput;
+  }
 
   switchMode(): void {
     SimpleViewComponent.app!.switchMode();
@@ -22,10 +56,31 @@ export class PowComponent {
   }
 
   async simulate() {
-    this.simText = '';
-    for (let i = 0; i < 20; i++) {
-      this.hashes.push(createBlockId());
-      await delay(100);
+    if (this.isRunning) {
+      return;
     }
+    this.isRunning = true;
+    this.hashes = [];
+    await this.createJob();
+  }
+
+  createJob(): Promise<string> {
+    return new Promise(async resolve => {
+      while (this.isRunning) {
+        const timeToWait = 1000 / this.hashRate;
+        const validationInput = this.validationInput;
+        for (let i = 0; i < this.hashRate; i++) {
+          const block = this.powService.createBlock(
+            validationInput[0], validationInput[1], 0, 0);
+          this.hashes.push(block.id);
+          if (block.isValid) {
+            this.isRunning = false;
+            break;
+          }
+          await delay(timeToWait);
+        }
+      }
+      resolve('done');
+    });
   }
 }
