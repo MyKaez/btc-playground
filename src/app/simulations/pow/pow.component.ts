e7e29cfa71afe795e0e68feb9d727c7780e9166c
Kenneth Sean Newall <KennethSean.Newall@arvato.com>
2022-08-19 10:03:17 +0200

    colorizing tabs

--- src/app/simulations/pow/pow.component.ts
+++ src/app/simulations/pow/pow.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-pow',
   templateUrl: './pow.component.html',
-  styleUrls: ['../../app.component.scss', '../../materials.scss']
+  styleUrls: ['../../app.component.scss', '../../materials.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PowComponent implements OnInit {
 
