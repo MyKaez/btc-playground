cbf698950bfe59b9c73d167667e017bf37220de2
Nico Dübeler <nico.duebeler@bertelsmann.de>
2022-09-28 20:02:18 +0200

    added structure

---
+++ src/prototypes/node-model/node-model.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'node-model',
+    templateUrl: 'node-model.component.html'
+})
+
+export class NodeModel implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
