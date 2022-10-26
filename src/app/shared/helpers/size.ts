1f1e0917b892fa5354772c44240686bb8a55fa98
Kenneth Newall <kennethsean.newall@arvato.com>
2022-10-26 16:39:05 +0200

    adjusted size

--- src/app/shared/helpers/size.ts
+++ src/app/shared/helpers/size.ts
@@ -1,10 +1,40 @@
-export function calculateSize(bytes: number): string {
-    let sizes = ['bytes', 'kilobytes', 'megabytes', 'gigabytes', 'terabytes', 'petabytes', 'exabytes'];
-    for (let i in sizes) {
+export function calculateSize(bytes: number): Size {
+    for (let i in Unit.units) {
         if (bytes < 1_000) {
-            return bytes + ' ' + sizes[i];
+            return new Size(bytes, Unit.units[i]);
         }
         bytes /= 1_000;
     }
-    return bytes + ' ' + sizes[sizes.length - 1];
+    return new Size(bytes, Unit.units[Unit.units.length - 1]);
+}
+
+export class Size {
+    size: number;
+    sizeUnit: Unit;
+    constructor(size: number, sizeUnit: Unit) {
+        this.size = size;
+        this.sizeUnit = sizeUnit;
+    }
+    toText(): string {
+        return this.size + ' ' + this.sizeUnit.text;
+    }
+}
+
+export class Unit {
+    multiplicator: number;
+    text: string;
+    private constructor(multiplicator: number, text: string) {
+        this.multiplicator = multiplicator;
+        this.text = text;
+    }
+    static readonly bytes = new Unit(0, 'bytes');
+    static readonly kiloBytes = new Unit(1, 'kilobytes');
+    static readonly megaBytes = new Unit(2, 'megabytes');
+    static readonly gigaBytes = new Unit(3, 'gigabytes');
+    static readonly teraBytes = new Unit(4, 'terabytes');
+    static readonly petaBytes = new Unit(5, 'petabytes');
+    static readonly exaBytes = new Unit(6, 'exabytes');
+    static readonly units = [
+        this.bytes, this.kiloBytes, this.megaBytes, this.gigaBytes, this.teraBytes, this.petaBytes, this.exaBytes
+    ];
 }
