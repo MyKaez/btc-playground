535907a87b66ab1c12401728216c8af92f99f1a1
Kenneth Newall <kennethsean.newall@arvato.com>
2022-10-26 14:03:24 +0200

    fixed time calculations

--- src/app/shared/helpers/time.ts
+++ src/app/shared/helpers/time.ts
@@ -2,20 +2,20 @@ export function calculateTime(seconds: number): string {
     if (seconds < 60) {
         return seconds.toFixed(0).padStart(2, '0') + ' Sekunden';
     }
-    let minutes = seconds / 60;
+    let minutes = Math.floor(seconds / 60);
     seconds = seconds % 60;
     if (minutes < 60) {
         return minutes.toFixed(0).padStart(2, '0') + ':' + seconds.toFixed(0).padStart(2, '0') + ' Minuten';
     }
-    let hours = minutes / 24;
-    minutes = minutes % 24;
+    let hours = Math.floor(minutes / 60);
+    minutes = minutes % 60;
     if (hours < 24) {
         return hours.toFixed(0).padStart(2, '0') + ':' + minutes.toFixed(0).padStart(2, '0') + ':' + seconds.toFixed(0).padStart(2, '0') + ' Stunden';
     }
-    let days = hours / 365.25;
-    hours = hours % 365.25;
-    if (days < 365) {
-        return days.toFixed(0) + ' Tagen';
+    let days = Math.floor(hours / 24);
+    hours = hours % 24;
+    if (days < 365.25) {
+        return days.toFixed(0) + ' Tagen ' + hours.toFixed(0) + ' Stunden';
     }
     let years = days / 365.25;
     days = days % 365.25;
