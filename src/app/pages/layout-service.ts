f4e84122a585742cfeaffb5ff81c040709ff1ba6
Kenneth Newall <kennethsean.newall@arvato.com>
2022-11-03 14:17:09 +0100

    adjusted background pic

--- src/app/pages/layout-service.ts
+++ src/app/pages/layout-service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class LayoutService {
     currentLayoutMode = ContentLayoutMode.ImageCarousel;
-    lockedImage: string = "assets/img/fixed-cloud.png";
+    lockedImage: string = "assets/img/fixed-crystals.png";
 
     setLayoutMode(mode: ContentLayoutMode) {
         this.currentLayoutMode = mode;
