bebd1e4a57816e23029e738edfd9514d89ef4f04
Kenneth Newall <kennethsean.newall@arvato.com>
2022-11-04 15:15:28 +0100

    adjusted prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: true
 };
+
+export class Config {
+  static readonly VOLTAGE_KEY: string = "api-key";
+  static readonly BTC_PAY_AUTH: string = "basic auth";
+  static readonly FIRE_REQUEST: boolean = false;
+}
