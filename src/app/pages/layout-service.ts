754f12e623a256579a454cf0974c24b123f2c403
Kenneth Newall <kennethsean.newall@arvato.com>
2022-12-07 19:15:32 +0100

    isHandset in LayoutService

--- src/app/pages/layout-service.ts
+++ src/app/pages/layout-service.ts
@@ -1,10 +1,22 @@
 import {Injectable} from "@angular/core";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {map, shareReplay} from "rxjs/operators";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class LayoutService {
   currentLayoutMode = ContentLayoutMode.ImageCarousel;
   lockedImage: string = "assets/img/fixed-crystals.png";
   contentWrapperClass: string = 'content-wrapper';
+  isHandset: Observable<boolean>;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.isHandset = breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      );
+  }
 
   setLayoutMode(mode: ContentLayoutMode) {
     this.currentLayoutMode = mode;
