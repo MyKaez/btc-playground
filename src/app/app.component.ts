47ba709daf0b7ba1cab46d066028e27b4a5097a8
Kenneth Newall <kennethsean.newall@arvato.com>
2023-01-30 16:58:45 +0100

    added presentation mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  get presentationMode(): string {
+    if (environment.presentationMode) {
+      return 'presentation-mode';
+    }
+    return '';
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  presentationMode: false
+  presentationMode: true
 };
 
 export class Config {
