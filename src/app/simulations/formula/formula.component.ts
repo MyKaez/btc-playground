148267ae388f8f06a41a9382a30ca1bce4ce69ab
Kenneth Newall <kennethsean.newall@arvato.com>
2023-02-05 11:45:08 +0100

    create table in formula console

--- src/app/simulations/formula/formula.component.ts
+++ src/app/simulations/formula/formula.component.ts
@@ -16,8 +16,7 @@ interface Halving {
   styleUrls: ['./formula.component.scss']
 })
 export class FormulaComponent implements AfterViewInit {
-  readonly separator: string = '|';
-  readonly columns = ['Nr', 'Coins pro Block', 'Coins im Halving', 'Gesamtanzahl Coins '];
+  readonly displayedColumns = ['number', 'coinsPerBlock', 'coinsPerHalving', 'totalCoins '];
 
   contentLayoutMode = ContentLayoutMode.LockImage;
 
@@ -68,37 +67,13 @@ export class FormulaComponent implements AfterViewInit {
     });
   }
 
-  public get header(): string {
-    let header = '';
-    for (let c of this.columns) {
-      header += ' ' + c.padEnd(c.length, '.') + ' ' + this.separator;
-    }
-    header = header.substring(0, header.length - this.separator.length);
-    return header;
+  getValue(halving: any, column: string) {
+    return halving[column];
   }
 
-  public get headerLine(): string {
-    const length = this.columns
-      .map(c => c.length + this.separator.length)
-      .reduce((prev, cur,) => prev + cur)
-      - 1;
-    return ''.padEnd(length, '-');
-  }
-
-  getString(halving: Halving) {
-    const addStartPadding = (val: string) => val.split('.')[0].length == 1 ? '0' + val : val;
-    const num = addStartPadding(halving.number.toString());
-    const blockCoins = addStartPadding(halving.coinsPerBlock.toFixed(8));
-    const halvingCoins = halving.coinsPerHalving.toFixed(8);
-    const totalCoins = halving.totalCoins.toFixed(8);
-    return `${num} | ${blockCoins} | ${halvingCoins} | ${totalCoins}`;
-  }
 
   getData(halvings: Halving[]) {
     return halvings[halvings.length - 1].totalCoins;
   }
-}
-function keys<T>() {
-  throw new Error('Function not implemented.');
 }
 
