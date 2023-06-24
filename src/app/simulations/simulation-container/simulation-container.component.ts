ca8bb126dd00c60ce878a3c30cf2d4a2dbd4527f
Kenneth Newall <kennethsean.newall@arvato.com>
2023-06-24 09:49:10 +0200

    created generic "swith tabs"

--- src/app/simulations/simulation-container/simulation-container.component.ts
+++ src/app/simulations/simulation-container/simulation-container.component.ts
@@ -29,12 +29,25 @@ export class SimulationContainerComponent implements OnInit {
       const currentRoute = `${parts[0]}/${parts[1]}`;
       const sim = simulations.find(sim => sim.navigationLink === currentRoute);
       console.log(`current simulation: ${sim?.title} `);
+      const tab = parts[2];
+      if (tab) {
+        if (tab === 'definition') {
+          this.toDefinition(this);
+        } else if (tab === 'simulation') {
+          this.toSimulation(this);
+        } else if (tab === 'sessions') {
+          this.toSessions(this);
+        } else if (tab === 'options') {
+          this.toOptions(this);
+        }
+      }
+
       return sim;
     })
   );
-
   showImageContainer$ = this.simulation$.pipe(map(sim => sim?.imageSrc || sim?.youtubeSrc));
   showVideo$ = this.simulation$.pipe(map(sim => sim?.youtubeSrc && !sim?.imageSrc));
+
   constructor(public layout: LayoutService, private simulationService: SimulationService,
     private router: Router) {
     this.isHandset$ = layout.isHandset$;
@@ -66,13 +79,23 @@ export class SimulationContainerComponent implements OnInit {
     }
   }
 
+  toDefinition(container: SimulationContainerComponent): void {
+    console.log('switch to definition!');
+    container.selectedTabIndex = 0;
+  }
+
   toSimulation(container: SimulationContainerComponent): void {
     console.log('switch to simulation!');
     container.selectedTabIndex = 1;
   }
 
+  toSessions(container: SimulationContainerComponent): void {
+    console.log('switch to session!');
+    container.selectedTabIndex = 2;
+  }
+
   toOptions(container: SimulationContainerComponent): void {
     console.log('switch to options!');
-    container.selectedTabIndex = 2;
+    container.selectedTabIndex = 3;
   }
 }
