faa4b70db518f5f2a3344f28f9d45f7cd4bf13d4
Kenneth Newall <kennethsean.newall@arvato.com>
2023-06-26 10:05:22 +0200

    show config content

--- src/app/pages/sessions/ui/config-info/config-info.component.ts
+++ src/app/pages/sessions/ui/config-info/config-info.component.ts
@@ -1,12 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-config-info',
   templateUrl: './config-info.component.html',
   styleUrls: ['./config-info.component.scss']
 })
-export class ConfigInfoComponent {
+export class ConfigInfoComponent implements OnInit {
 
-  @Input("config") config!: any
+  @Input("config") config!: any;
+
+  keys: string[] = [];
+
+  ngOnInit(): void {
+    this.keys = Object.keys(this.config);
+  }
 
 }
