d10a20cfa88d7b5c16cd6999b4021ecff6a5f2cf
Kenneth Newall <kennethsean.newall@arvato.com>
2023-06-26 09:54:21 +0200

    added config info

---
+++ src/app/pages/sessions/ui/config-info/config-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-config-info',
+  templateUrl: './config-info.component.html',
+  styleUrls: ['./config-info.component.scss']
+})
+export class ConfigInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
