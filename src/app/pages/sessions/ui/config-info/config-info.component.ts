bb7ed70d3da4bb73129d7955d666d7089d9fb899
Kenneth Newall <kennethsean.newall@arvato.com>
2023-07-07 12:59:12 +0200

    added "edit" capabilities to config-info

--- src/app/pages/sessions/ui/config-info/config-info.component.ts
+++ src/app/pages/sessions/ui/config-info/config-info.component.ts
@@ -10,9 +10,22 @@ export class ConfigInfoComponent implements OnInit {
   @Input("config") config!: any;
 
   keys: string[] = [];
+  infos: ConfigInfo[] = [
+    { property: 'secondsUntilBlock', editable: true, sendData: true },
+    { property: 'totalHashRate', editable: false, sendData: true },
+  ]
+
+  isReadonly(property: string): boolean {
+    return !(this.infos.find(info => info.property === property)?.editable ?? false);
+  }
 
   ngOnInit(): void {
     this.keys = Object.keys(this.config);
   }
 
+}
+export interface ConfigInfo {
+  property: string;
+  editable: boolean;
+  sendData: boolean;
 }
