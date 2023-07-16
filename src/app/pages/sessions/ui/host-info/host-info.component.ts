809da993db97728cbdb63b08872f3471559f47f9
Pandabyts <nic.duebeler@gmail.com>
2023-07-16 12:51:43 +0200

    designed status display as chips

--- src/app/pages/sessions/ui/host-info/host-info.component.ts
+++ src/app/pages/sessions/ui/host-info/host-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { SessionStatusDisplayValues } from 'src/app/models/session';
 import { ViewModel } from 'src/app/models/view-model';
 
 @Component({
@@ -10,5 +11,7 @@ export class HostInfoComponent {
 
   @Input("vm") vm!: ViewModel;
 
-
+  get sessionStatusDisplayValue(): string {
+    return SessionStatusDisplayValues[this.vm.session.status];
+  }
 }
