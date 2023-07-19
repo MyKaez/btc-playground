b2abd917ed4bbc9ef5ac13c8871ed9e0a1c9219a
Kenneth Newall <kennethsean.newall@arvato.com>
2023-07-19 16:14:23 +0200

    added progress bar with colorin

--- src/app/pages/sessions/ui/hash-progress/hash-progress.component.ts
+++ src/app/pages/sessions/ui/hash-progress/hash-progress.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { filter, interval, map, of, shareReplay, takeUntil, takeWhile, tap } from 'rxjs';
+import { SessionInfo } from 'src/app/models/session';
+import { PowConfig } from 'src/app/simulations/pow/models/pow-config';
 
 @Component({
   selector: 'app-hash-progress',
   templateUrl: './hash-progress.component.html',
   styleUrls: ['./hash-progress.component.scss']
 })
-export class HashProgressComponent implements OnInit {
+export class HashProgressComponent {
 
-  constructor() { }
+  @Input('session') session!: SessionInfo;
 
-  ngOnInit(): void {
+  get pow(): PowConfig {
+    return <PowConfig>this.session.configuration;
   }
 
+  get estimatedHashes(): number {
+    return this.pow.secondsUntilBlock * this.pow.totalHashRate;
+  }
+
+  time$ = interval(1_000).pipe(
+    takeWhile(_ => this.session.status === 'started'),
+    shareReplay(1)
+  );
+
+  hashes$ = this.time$.pipe(
+    map(second => {
+      const hashes = this.pow.totalHashRate * second;
+      return hashes;
+    })
+  );
+
+  progress$ = this.time$.pipe(
+    map(_ => {
+      const pow = <PowConfig>this.session.configuration;
+      const max = pow.secondsUntilBlock;
+      const time = new Date().getTime() - this.session.startTime!.getTime();
+      const seconds = time / 1000;
+      return seconds / max * 100;
+    })
+  );
 }
