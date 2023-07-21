391a425f99bcf76d8046d0acbe301ca56f2fa326
Pandabyts <nic.duebeler@gmail.com>
2023-07-21 09:16:16 +0200

    added sorting of users by found hash

--- src/app/models/view-model.ts
+++ src/app/models/view-model.ts
@@ -1,10 +1,13 @@
 import { HubConnection } from "@microsoft/signalr";
+import { deprecate } from "util";
 import { SessionInfo } from "./session";
 import { UserControl } from "./user";
 
 export class ViewModel {
 
-    constructor(public session: SessionInfo, public connection: HubConnection) { }
+    constructor(public session: SessionInfo, public connection: HubConnection) {
+        this.sortUsersInSession(session);
+     }
 
     user?: UserControl;
     userUpdates: (() => void)[] = [];
@@ -16,5 +19,12 @@ export class ViewModel {
     onUsersUpdate() {
         console.log(`running ${this.userUpdates.length} updates`);
         this.userUpdates.forEach(update => update());
+    }
+
+    /** @deprecated move into streams of session */
+    private sortUsersInSession(session: SessionInfo) {
+        if(!session.users?.length) return;
+
+        session.users.sort((left, right) => left.configuration?.hash && !right.configuration?.hash ? -1 : 0);
     }
 }
